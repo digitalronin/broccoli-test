57ba2b3c023b916f21e31365366d127e30ad9415
David Salgado <david@digitalronin.com>
2016-01-23 15:12:03 +0000

    use ES6 generator function

--- Brocfile.js
+++ Brocfile.js
@@ -1,14 +1,25 @@
-var babel  = require('broccoli-babel-transpiler');
-var funnel = require('broccoli-funnel');
-var concat = require('broccoli-concat');
+var babel      = require('broccoli-babel-transpiler')
+var funnel     = require('broccoli-funnel')
+var concat     = require('broccoli-concat')
+var mergeTrees = require('broccoli-merge-trees')
 
 var appJs = babel('src')
 
+// // Grab the polyfill file provided by the Babel library
+var babelPath = 'node_modules/babel-core'
+
+var browserPolyfill = funnel(babelPath, {
+  files: ['browser-polyfill.js']
+})
+
+// Add the Babel polyfill to the tree of transpiled files
+appJs = mergeTrees([browserPolyfill, appJs])
+
 // Concatenate all the JS files into a single file
 appJs = concat(appJs, {
-  inputFiles: ['*.js'],
+  inputFiles: ['browser-polyfill.js', '*.js'],
   outputFile: 'index.js'
-});
+})
 
 module.exports = appJs
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
+function* counter(start, stop) {
+  for (var i = start; i < stop; i += 1) {
+    yield i;
+  }
+}
+
 (() => {
     console.log("Hello, ES6 world!")
 })()
+
+for (let i of counter(1, 5)) {
+  console.log(`count is ${i}`)
+}
 
